4722c2da5aea539d066ee6b425d1d541879171e7
agent7799 <84741888+agent7799@users.noreply.github.com>
2021-11-14 02:34:09 +0300

    Java Core: 7 lection 10 task 1711

--- 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
@@ -36,78 +36,76 @@ public class Solution {
 
 
         switch (args[0]) {
-            case ("-c") :
-            synchronized (allPeople)
-            {
-                for( int i = 1; i< args.length; i+=3){
+            case ("-c"):
+                synchronized (allPeople) {
+                    for (int i = 1; i < args.length; i += 3) {
 //                    addPerson(args[i+1], args[i+2], args[i+3]);
 //                    System.out.println(allPeople.get(i));
-                    String name = args[i];
-                    Date birthDate = dateFormatIn.parse(args[i+2]);
-                    String sex = args[i+1];
-                    Person person = sex.equals("м") ? Person.createMale(name, birthDate) : Person.createFemale(name, birthDate);
-                    allPeople.add(person);
-                    System.out.println(allPeople.indexOf(person));
+                        String name = args[i];
+                        Date birthDate = dateFormatIn.parse(args[i + 2]);
+                        String sex = args[i + 1];
+                        Person person = sex.equals("м") ? Person.createMale(name, birthDate) : Person.createFemale(name, birthDate);
+                        allPeople.add(person);
+                        System.out.println(allPeople.indexOf(person));
+                    }
+                    break;
                 }
-                break;
-            }
 
-            case ("-u") :
-            synchronized (allPeople)
-            {
-                for( int i = 1; i< args.length; i+=4){
+            case ("-u"):
+                synchronized (allPeople) {
+                    for (int i = 1; i < args.length; i += 4) {
 //                    updatePerson(args[i+1], args[i+2], args[i+3], args[4]);
 //                    System.out.println(allPeople.get(i));
-                    int id = Integer.parseInt(args[i]);
-                    String name = args[i+1];
-                    String sex = args[i+3];
-                    Date birthDate = dateFormatIn.parse(args[i+3]);
-                    Person currentPerson = allPeople.get(id);
-                    currentPerson.setSex(sex.equals("м") ? Sex.MALE : Sex.FEMALE);
-                    currentPerson.setName(name);
-                    currentPerson.setBirthDate(birthDate);
+                        int id = Integer.parseInt(args[i]);
+                        String name = args[i + 1];
+                        String sex = args[i + 3];
+                        Date birthDate = dateFormatIn.parse(args[i + 3]);
+                        Person currentPerson = allPeople.get(id);
+                        currentPerson.setSex(sex.equals("м") ? Sex.MALE : Sex.FEMALE);
+                        currentPerson.setName(name);
+                        currentPerson.setBirthDate(birthDate);
+                    }
                 }
-            }
-            case ("-d") :
-            synchronized (allPeople)
-            {
-                for( int i = 1; i< args.length; i++){
-                   //deletePerson(i);
-                    Person currentPerson = allPeople.get(Integer.parseInt(args[i+1]));
-                    currentPerson.setBirthDate(null);
-                    currentPerson.setName(null);
-                    currentPerson.setSex(null);
+            case ("-d"):
+                synchronized (allPeople) {
+                    for (int i = 1; i < args.length; i++) {
+                        //deletePerson(i);
+                        Person currentPerson = allPeople.get(Integer.parseInt(args[i + 1]));
+                        currentPerson.setBirthDate(null);
+                        currentPerson.setName(null);
+                        currentPerson.setSex(null);
 
+                    }
                 }
-            }
-            case ("-i") :
-            synchronized (allPeople)
-            {
-                for( int i = 1; i< args.length; i++){
+            case ("-i"):
+                synchronized (allPeople) {
+                    for (int i = 1; i < args.length; i++) {
 //                    printPerson(i);
 
-                    int id = Integer.parseInt(args[i+1]);
-                    Person currentPerson = allPeople.get(id);
-                    String name = currentPerson.getName();
-                    String sex = currentPerson.getSex()==Sex.MALE ? "м" : "ж";
-                    String birthDate = dateFormatOut.format(currentPerson.getBirthDate());
-                    System.out.println(name + sex + birthDate);
+                        int id = Integer.parseInt(args[i + 1]);
+                        Person currentPerson = allPeople.get(id);
+                        String name = currentPerson.getName();
+                        String sex = currentPerson.getSex() == Sex.MALE ? "м" : "ж";
+                        String birthDate = dateFormatOut.format(currentPerson.getBirthDate());
+                        System.out.println(name + sex + birthDate);
+                    }
                 }
-            }
             default:
-            synchronized (allPeople){
+                synchronized (allPeople) {
 
-            }
+                }
         }
+    }
+}
+
 //
 //
-   }
 //        //напишите тут ваш код
-
-
-        private static Sex getSexFromString (String s){
-            return s.equals("м") ? Sex.MALE : Sex.FEMALE;
-        }
+//
+//
+//        private static Sex getSexFromString (String s){
+//            return s.equals("м") ? Sex.MALE : Sex.FEMALE;
+//        }
 //
 //        private static void addPerson (String name, String sex, String birthDate) throws ParseException {
 //            if (getSexFromString(sex) == Sex.MALE) {
@@ -117,14 +115,14 @@ public class Solution {
 //            }
 //            System.out.println(allPeople.size() - 1);
 //        }
-
-
-        private static void deletePerson ( int id){
-            Person currentPerson = allPeople.get(id);
-            currentPerson.setBirthDate(null);
-            currentPerson.setName(null);
-            currentPerson.setSex(null);
-        }
+//
+//
+//        private static void deletePerson ( int id){
+//            Person currentPerson = allPeople.get(id);
+//            currentPerson.setBirthDate(null);
+//            currentPerson.setName(null);
+//            currentPerson.setSex(null);
+//        }
 
 //        private static void updatePerson (String id, String name, String sexString, String birthDate) throws
 //        ParseException {
@@ -143,5 +141,3 @@ public class Solution {
 //                    " " + dateFormatOut.format(currentPerson.getBirthDate()));
 //        }
 
-
-    }
