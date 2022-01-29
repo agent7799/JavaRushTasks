62f37ff174ad77396ac4e9ae0b14fbeea4127fc3
agent7799 <84741888+agent7799@users.noreply.github.com>
2022-01-30 00:22:55 +0300

    task 2003 Знакомство с properties

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -1,8 +1,7 @@
 package com.javarush.task.task20.task2003;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /* 
 Знакомство с properties
@@ -14,19 +13,39 @@ public class Solution {
 
     public static void save(OutputStream outputStream) throws Exception {
         //напишите тут ваш код
+        Properties properties = new Properties();
+        for (Map.Entry<String, String> entry : runtimeStorage.entrySet()){
+           properties.setProperty(entry.getKey(), entry.getValue());
+        }
+        properties.store(outputStream, "app prop");
+
     }
 
     public static void load(InputStream inputStream) throws IOException {
         //напишите тут ваш код
+    Properties properties = new Properties();
+    properties.load(inputStream);
+
+
+        for (Map.Entry<Object, Object> map : properties.entrySet()){
+            runtimeStorage.put((String) map.getKey(), (String) map.getValue());
+        }
+
+
     }
 
     public static void main(String[] args) {
+
+        String path = "c:\\java\\app.properties";
+
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-             FileInputStream fos = new FileInputStream(reader.readLine())) {
+           FileInputStream fos = new FileInputStream(reader.readLine())) {
+          // FileInputStream fos = new FileInputStream(path) {     //   String path = "c:\\java\\app.properties";
             load(fos);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
 
         System.out.println(runtimeStorage);
     }
