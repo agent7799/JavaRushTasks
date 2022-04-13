cfee2c1ce0df67a4e957c8e5cea59e850f2c4e50
Agent7799 <agent7799@mail.ru>
2022-04-13 23:42:10 +0300

    com.javarush.task.task21.task2113 Hippodreme part 6

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,10 +1,14 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
 
-private List<Horse> horses;
+    private List<Horse> horses;
+    static Hippodrome game;
+
+
 
     public List<Horse> getHorses() {
         return horses;
@@ -14,7 +18,14 @@ private List<Horse> horses;
         this.horses = horses;
     }
 
+
+
     public static void main(String[] args) {
-        
+        ArrayList<Horse> horses = new ArrayList<>();
+        game = new Hippodrome(horses);
+
+        horses.add(new Horse("Belka", 3, 0));
+        horses.add(new Horse("Strelka", 3, 0));
+        horses.add(new Horse("Venera", 3, 0));
     }
 }
