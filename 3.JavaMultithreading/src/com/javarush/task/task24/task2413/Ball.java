6986ec5c694c69a466899a7cfad6f99d320bd8b6
Agent7799 <agent7799@mail.ru>
2022-04-30 00:48:34 +0300

    package com.javarush.task.task23.task2413 Arkanoid (16/20)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,17 +1,53 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject{
+    private double speed;
+    private  double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        isFrozen = true;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
     }
 
     @Override
     void draw(Canvas canvas) {
-
+        canvas.setPoint(x, y, 'O');
     }
 
     @Override
     void move() {
+       if (!isFrozen){
+           x +=dx;
+           y +=dy;
+       }
+    }
 
+    void start(){
+        isFrozen = false;
     }
 }
