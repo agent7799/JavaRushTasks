bb45ad52008ddd9cb38da8fcc24195fdcab779b3
Agent7799 <agent7799@mail.ru>
2022-05-08 15:31:16 +0300

    package com.javarush.task.task23.task2508

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task25.task2508;
+
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
+    Thread thread = new Thread();
+
+    public TaskManipulator() {
+    }
+
+    @Override
+        public void run() {
+        Thread current = Thread.currentThread();
+            while (!current.isInterrupted()){
+                System.out.println(current.getName());
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    /** если нить уже закончила работу, то вызов Thread.sleep(100) выбросит
+                        сключение, поэтому break
+                        break;
+                    */
+                    break;
+                }
+            }
+        }
+
+
+    @Override
+    public void start(String threadName) {
+        thread = new Thread(this, threadName);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
+    }
+}
