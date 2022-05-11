6d276f161b0a8f0ef92deb480271905c111bc9e8
Agent7799 <agent7799@mail.ru>
2022-05-11 21:45:47 +0300

    fixup! package com.javarush.task.task23.task2909 Multithreading final task: refactoring (10/15)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -10,31 +10,42 @@ public class Human implements Alive{
     protected int age;
     protected String name;
 
+    public class Size {
+        public   int height;
+        public   int weight;
 
-    protected int[] size;
+        public Size(int height, int weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+    }
+
+
+
+    protected Size size;
 
     //protected boolean isSoldier;
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+//    public static final int FIRST = 1;
+//    public static final int SECOND = 2;
+//    public static final int THIRD = 3;
+//    public static final int FOURTH = 4;
+
+   private BloodGroup bloodGroup;
 
     private List<Human> children = new ArrayList<>();
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public void setBloodGroup(BloodGroup code) {
+       bloodGroup = code;
     }
 
-    public int getBloodGroup() {
+    public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
 
     public Human(String name, int age) {
         this.name = name;
         this.age = age;
-
     }
 
     public List<Human> getChildren() {
@@ -82,6 +93,6 @@ public class Human implements Alive{
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 }
