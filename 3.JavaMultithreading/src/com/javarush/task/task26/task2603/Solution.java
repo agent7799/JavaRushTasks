8cd412d3990505a8ad7f9838e393fb5d12631014
Agent7799 <agent7799@mail.ru>
2022-05-25 10:22:14 +0300

    fixup! package com.javarush.task.task23.task2603 сортировать ассивом компараторов CustomizedComparator(Comparator<T>... comparators)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task26.task2603;
+
+/*
+Убежденному убеждать других не трудно
+*/
+
+import java.util.Comparator;
+
+public class Solution {
+
+
+    public static class CustomizedComparator<T> implements Comparator<T>{
+        private Comparator<T>[] comparators;
+
+        public CustomizedComparator(Comparator<T>... comparators) {
+            if(comparators.length > 0) {
+                this.comparators = comparators;
+            }
+        }
+
+        @Override
+        public int compare(T o1, T o2) {
+            int result = 0;
+            for (Comparator<T> comparator : comparators) {
+                result = comparator.compare(o1, o2);
+                if (result != 0){
+                   return result;
+                }
+            }
+            return result;
+        }
+    };
+
+
+
+    public static void main(String[] args) {
+    }
+
+}
