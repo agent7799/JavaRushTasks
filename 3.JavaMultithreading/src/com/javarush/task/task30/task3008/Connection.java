e2e9907d70556ea1c0f39bf9f42446272665fbe2
Agent7799 <agent7799@mail.ru>
2022-05-29 20:55:04 +0300

    3008 chat (5/22)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,4 +1,48 @@
 package com.javarush.task.task30.task3008;
 
-public class Connection {
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Connection implements Closeable {
+
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    void send(Message message) throws IOException{
+        synchronized (out){
+            out.writeObject(message);
+        }
+
+    }
+
+    Message receive() throws IOException, ClassNotFoundException{
+        synchronized (in){
+            Message message = (Message) in.readObject();
+            return message;
+        }
+
+    }
+
+    SocketAddress getRemoteSocketAddress(){
+        SocketAddress socketAddress = this.socket.getRemoteSocketAddress();
+        return socketAddress;
+    }
+
+    public void close() throws IOException{
+        in.close();
+        out.close();
+        socket.close();
+    }
+
 }
