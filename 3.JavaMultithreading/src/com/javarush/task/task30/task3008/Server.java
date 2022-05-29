a83f14982a0c237bde4685771d78f84b7255dfbd
Agent7799 <agent7799@mail.ru>
2022-05-29 21:16:12 +0300

    3008 chat (6/22)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,9 +1,43 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+    private static class Handler extends  Thread{
+        private Socket socket;
+
+        public Handler(Socket socket){
+            this.socket = socket;
+        }
+
+    }
 
     public static void main(String[] args) {
+        int port = ConsoleHelper.readInt();
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server Started!");
+            while (true){
+                    Socket socket = serverSocket.accept();
+                    Handler handler = new Handler(socket);
+                    handler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 
     }
 }
