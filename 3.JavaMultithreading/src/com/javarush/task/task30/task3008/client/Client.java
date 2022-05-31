a681f2ffbcc261deff4f5d27265317fd21481543
Agent7799 <agent7799@mail.ru>
2022-05-31 10:29:56 +0300

    3008 chat (14/22)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -10,40 +10,79 @@ public class Client {
     private volatile boolean clientConnected = false;
 
 
-    public class SocketThread extends Thread{
+    public class SocketThread extends Thread {
 
     }
 
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
 
-    protected String getServerAddress(){
+
+    protected String getServerAddress() {
         return ConsoleHelper.readString();
     }
 
-    protected int getServerPort(){
+    protected int getServerPort() {
         return ConsoleHelper.readInt();
     }
 
-    protected String getUserName(){
+    protected String getUserName() {
         return ConsoleHelper.readString();
     }
 
-    protected boolean shouldSendTextFromConsole(){
+    protected boolean shouldSendTextFromConsole() {
         return true;
     }
 
-    protected SocketThread getSocketThread(){
+    protected SocketThread getSocketThread() {
         return new SocketThread();
     }
 
-    protected void sendTextMessage(String text){
-        try{
+    protected void sendTextMessage(String text) {
+        try {
             Message message = new Message(MessageType.TEXT, text);
             connection.send(message);
-        }catch (IOException e){
+        } catch (IOException e) {
             clientConnected = false;
             ConsoleHelper.writeMessage("Error sending message");
         }
 
     }
+
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+
+            try {
+                synchronized (this) {
+                this.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (clientConnected) {
+                ConsoleHelper.writeMessage("Соединение установлено.\n" +
+                        "Для выхода наберите команду 'exit'.");
+            } else {
+                ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+            }
+
+
+        while (clientConnected) {
+            String text = ConsoleHelper.readString();
+            if (text.equalsIgnoreCase("exit")) {
+                break;
+            }
+            if (shouldSendTextFromConsole()) {
+                sendTextMessage(text);
+            }
+        }
+
+
+    }
+
 
 }
