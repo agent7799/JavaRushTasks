37a9d4de69bdc79a9a8c93d107286adfa24c8c2d
Agent7799 <agent7799@mail.ru>
2022-06-10 20:16:25 +0300

    3008 chat (19/22)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,8 +1,53 @@
 package com.javarush.task.task30.task3008.client;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class BotClient extends Client{
 
     public class BotSocketThread extends SocketThread{
+
+        @Override
+        protected  void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+                super.clientMainLoop();
+        }
+
+        @Override
+        protected void processIncomingMessage(String message) {
+            super.processIncomingMessage(message);
+            System.out.println(message);
+            if(message.contains(": ")){
+                String name = message.substring(0, message.indexOf(": "));
+                String text = message.substring(message.indexOf(": ") + 2);
+                SimpleDateFormat dateFormat;
+                String responseText;
+                Date date = Calendar.getInstance().getTime();
+                switch (text) {
+                    case "дата" :
+                        dateFormat = new SimpleDateFormat("d.MM.YYYY");
+                        responseText = dateFormat.format(date);
+                        sendTextMessage("Информация для " + name + ": " + responseText + ".");
+                    case "день" :
+                        dateFormat = new SimpleDateFormat("d");
+                    case "месяц" :
+                        dateFormat = new SimpleDateFormat("MMMM");
+                    case "год" :
+                        dateFormat = new SimpleDateFormat("YYYY");
+                    case "время" :
+                        dateFormat = new SimpleDateFormat("H:mm:ss");
+                    case "час" :
+                        dateFormat = new SimpleDateFormat("H");
+                    case "минуты" :
+                        dateFormat = new SimpleDateFormat("m");
+                    case "секунды" :
+                        dateFormat = new SimpleDateFormat("s");
+                }
+            }
+
+        }
     }
 
     @Override
