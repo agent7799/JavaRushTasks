4fac7a051fa17d7cb83e20aa3db45bbbf7536248
Agent7799 <agent7799@mail.ru>
2022-06-21 10:04:28 +0300

    3110 Archiver (8/18) menu handling

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,31 +1,50 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.nio.file.Paths;
-import java.util.Scanner;
-
+import java.io.IOException;
 
 
 public class Archiver {
 
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Веедите операцию:");
+        for (Operation op : Operation.values()) {
+            ConsoleHelper.writeMessage(op.ordinal() + " - " + op.name());
+        }
+
+          return Operation.values()[ConsoleHelper.readInt()];
+    }
+
     public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(System.in);
-        ConsoleHelper.writeMessage("Введите полный путь к файлу архива: ");
-        String zipFile = scanner.nextLine();
-        zipFile = "c:\\Java\\test.zip";
-        ConsoleHelper.writeMessage("По умолчанию полный путь к файлу архива: " + zipFile);
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(zipFile) );
+//        Scanner scanner = new Scanner(System.in);
+//        ConsoleHelper.writeMessage("Введите полный путь к файлу архива: ");
+//        String zipFile = scanner.nextLine();
+//        zipFile = "c:\\Java\\test.zip";
+//        ConsoleHelper.writeMessage("По умолчанию полный путь к файлу архива: " + zipFile);
+//        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(zipFile));
+//
+//        ConsoleHelper.writeMessage("Введите полный путь к архивируемому файлу: ");
+//        String archivedFile = scanner.nextLine();
+//        archivedFile = "c:\\Java\\test.txt";
+//        ConsoleHelper.writeMessage("По умолчанию полный путь к архивируемому файлу: " + archivedFile);
+//        zipFileManager.createZip(Paths.get(archivedFile));
 
-        ConsoleHelper.writeMessage("Введите полный путь к архивируемому файлу: ");
-        String archivedFile = scanner.nextLine();
-        archivedFile = "c:\\Java\\test.txt";
-        ConsoleHelper.writeMessage("По умолчанию полный путь к архивируемому файлу: " + archivedFile);
-        zipFileManager.createZip(Paths.get(archivedFile));
 
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+        Operation operation = null;
+        while (operation != Operation.EXIT) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
+
     }
 
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Operation.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Operation.java
@@ -1,10 +1,18 @@
 package com.javarush.task.task31.task3110;
 
 public enum Operation {
-    CREATE,
-    ADD,
-    REMOVE,
-    EXTRACT,
-    CONTENT,
-    EXIT
-}
+    CREATE("упаковать файлы в архив"),
+    ADD("добавить файл в архив"),
+    REMOVE("удалить файл из архива"),
+    EXTRACT("распаковать архив"),
+    CONTENT("просмотреть содержимое архива"),
+    EXIT("выход");
+
+
+    String description;
+
+    Operation(String name) {
+        this.description = name;
+    }
+
+    }
