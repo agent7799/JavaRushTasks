f77d4e4f1ab86f344f2f6de1d2665f18d926096a
Agent7799 <agent7799@mail.ru>
2022-06-23 01:09:25 +0300

    3110 Archiver (12/18) menu handling + adding fileManager + ZipFileManager refactoring + properties

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -6,17 +6,11 @@ import com.javarush.task.task31.task3110.ZipFileManager;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public abstract class ZipCommand implements Command{
+public abstract class ZipCommand implements Command {
 
-    public ZipFileManager getZipFileManager() throws Exception{
-
-            ConsoleHelper.writeMessage("Введите полный путь к файлу архива: ");
-            String zipFile = ConsoleHelper.readString();
-//          zipFile = "c:\\Java\\test.zip";
-//          ConsoleHelper.writeMessage("По умолчанию полный путь к файлу архива: " + zipFile);
-            Path zipPath = Paths.get(zipFile);
-
-            return new ZipFileManager(zipPath);
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
-
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -7,22 +7,21 @@ import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class ZipCreateCommand extends ZipCommand{
+public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-        ZipFileManager manager = getZipFileManager();
         try {
             ConsoleHelper.writeMessage("Создание архива.");
-            ConsoleHelper.writeMessage("Введите полный путь к файлу или директории для архивации: ");
-            String fileToBeArchived = ConsoleHelper.readString();
-//            archivedFile = "c:\\Java\\test.txt";
-//            ConsoleHelper.writeMessage("По умолчанию полный путь к архивируемому файлу: " + fileToBeArchived);
 
-            Path path = Paths.get(fileToBeArchived);
+            ZipFileManager zipFileManager = getZipFileManager();
 
-            manager.createZip(path);
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(sourcePath);
+
             ConsoleHelper.writeMessage("Архив создан.");
-        }catch (PathIsNotFoundException e){
+
+        } catch (PathIsNotFoundException e) {
             ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
         }
     }
