80876c48bf87d9c0650f5fbc1eb8ec2a7e9a5ed3
Agent7799 <agent7799@mail.ru>
2022-07-07 20:25:40 +0300

    3209 HTML Editor (15/24) reset document

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 public class Controller {
@@ -25,11 +26,22 @@ public class Controller {
         controller.init();
     }
 
-    public void init(){
+    public void init() {
 
     }
 
-    public void exit(){
+    public void exit() {
         System.exit(0);
     }
+
+    public void resetDocument() {
+        if (document != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
+    }
+
+
 }
