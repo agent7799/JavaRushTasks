ccb480bc40d9853bb5f1c37c306b0e840c05b3e5
Agent7799 <agent7799@mail.ru>
2022-07-24 21:10:37 +0300

    3001 EPIC Numeric system converter!!!

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3001/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3001/Solution.java
@@ -17,10 +17,6 @@ public class Solution {
         result = convertNumberToOtherNumberSystem(number, NumberSystemType._8);
         System.out.println(result);    //expected 3337
 
-        number = new Number(NumberSystemType._16, "1234af");
-        result = convertNumberToOtherNumberSystem(number, NumberSystemType._16);
-        System.out.println(result);    //expected abcdefabcdef
-
         number = new Number(NumberSystemType._16, "abcdefabcdef");
         result = convertNumberToOtherNumberSystem(number, NumberSystemType._16);
         System.out.println(result);    //expected abcdefabcdef
@@ -35,53 +31,43 @@ public class Solution {
 
         String digits = "0123456789abcdef";
 
-        System.out.println();
-        System.out.println(numberString);
-        System.out.println("source number base " + numberSystem);
-        System.out.println("target number base " + m);
+//        System.out.println();
+//        System.out.println(numberString);
+//        System.out.println("source number base " + numberSystem);
+//        System.out.println("target number base " + m);
 
-        for (int i = 0; i < numberString.length(); i++) {
-            char c = numberString.toLowerCase().charAt(i);
-            if (digits.indexOf(c) > numberSystem - 1) {
-                System.out.println(digits.indexOf(c) + " > " + (numberSystem-1));
-                throw new NumberFormatException();
-            }
-        }
+        checkIfNumberValid(numberSystem, numberString, digits);
 
-        BigInteger A = new BigInteger(numberString.toUpperCase(), numberSystem);
+        BigInteger A = new BigInteger(numberString, numberSystem);
         BigInteger M = new BigInteger(m + "");
+        BigInteger divider = A.divide(M);
+        BigInteger reminder = A.remainder(M);
 
-        while (true) {
-            BigInteger divider = A.divide(M);
-            BigInteger reminder = A.remainder(M);
+       while (divider.compareTo(BigInteger.ZERO) > 0) {
 
-            if (reminder.compareTo(BigInteger.ZERO) > 0) {        //
-                System.out.println("делим " + A + "  на " + M + " = " + divider + " остаток " + reminder);
+                //System.out.println("делим " + A + "  на " + M + ": = " + divider + ", остаток " + reminder );
                 stringBuilder.append(digits.charAt(reminder.intValue()));
                 A = divider;
-            } else {
-                stringBuilder.append(digits.charAt(A.intValue()));
-                break;
-            }
+                divider = A.divide(M);
+                reminder = A.remainder(M);
         }
+        stringBuilder.append(digits.charAt(reminder.intValue()));
 
-//        CONVERTER
-
-//        while (true) {
-//            if (a / m > 0) {
-//                System.out.println("делим " + a + "  на " + m + " = " + a / m + " остаток " + a % m);
-//                stringBuilder.append(digits.charAt(a % m));
-//                a = a/m;
-//            } else {
-//                stringBuilder.append(digits.charAt(a));
-//                break;
-//            }
-//        }
-
+//
 
         stringBuilder.reverse();
         return new Number(expectedNumberSystem, stringBuilder.toString());
 
+    }
+
+    private static void checkIfNumberValid(int numberSystem, String numberString, String digits) {
+        for (int i = 0; i < numberString.length(); i++) {
+            char c = numberString.toLowerCase().charAt(i);
+            if (digits.indexOf(c) > numberSystem - 1) {
+                System.out.println(digits.indexOf(c) + " > " + (numberSystem -1));
+                throw new NumberFormatException();
+            }
+        }
     }
 
 //    public static int hexToDecimal(String s) {
@@ -95,4 +81,18 @@ public class Solution {
 //        }
 //        return val;
 //    }
+
 }
+
+  //   CONVERTER
+
+//        while (true) {
+//            if (a / m > 0) {
+//                System.out.println("делим " + a + "  на " + m + " = " + a / m + " остаток " + a % m);
+//                stringBuilder.append(digits.charAt(a % m));
+//                a = a/m;
+//            } else {
+//                stringBuilder.append(digits.charAt(a));
+//                break;
+//            }
+//        }
