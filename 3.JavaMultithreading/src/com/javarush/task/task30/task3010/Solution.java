34242b8778b3e7a59ff215606b957e0d886d7ff0
Agent7799 <agent7799@mail.ru>
2022-07-30 18:26:52 +0300

    3010 Минимальное допустимое основание системы счисления solved

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
@@ -2,64 +2,80 @@ package com.javarush.task.task30.task3010;
 
 /*
 Минимальное допустимое основание системы счисления
+
+//           00 expected 2
+//           12AS08z expected 36
+//           12AS08Z/ expected incorrect
+
 */
+
+import java.util.regex.Pattern;
 
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-        String test = "00";
-        String test1 = "12AS08z";
-        String test2 = "12AS08Z/";
-        // String test3 = args[0];
+        try {
+            // findMinRadix(args[0]);    //my solution
+            findSolution(args[0]);      //solution from JR
 
-        System.out.println(isStringCorrect(test));
-        System.out.println(isStringCorrect(test1));
-        System.out.println(isStringCorrect(test2));
-
-        //findMinRadix(args[0]);
-        System.out.println(findMinRadix(test));
-        System.out.println(findMinRadix(test1));
-        System.out.println(findMinRadix(test2));
-    }
-
-    private static boolean isStringCorrect(String s) {
-        if (s.length() > 255) {
-            return false;
-        } else if (s.matches("\\W")) {
-            System.out.println("not matches");
-            return false;
-        } else {
-
-            for (int a : s.toCharArray()) {
-                if (a < 32 || a > 127) {
-                    return false;
-                }
-            }
+//            findMinRadix("00");
+//            findSolution("00");
+//
+//            findMinRadix("12AS08z");
+//            findSolution("12AS08z");
+//
+//            findMinRadix("12AS08Z/");
+//            findSolution("12AS08Z/");
+        } catch (Exception e) {
         }
-        return true;
     }
 
-
-    private static String findMinRadix(String number) {
+    private static void findMinRadix(String number) {
         String digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String regex = "[^0-9a-zA-Z]";
 
-        if (!isStringCorrect(number)) {
+        String line = number;
+        Pattern pattern = Pattern.compile(regex);
+        if (pattern.matcher(line).find()) {
             System.out.println("incorrect");
-            return "incorrect";
-        }
-
-        int minRadix = digits.indexOf(number.toUpperCase().charAt(0));
-        for (int i = 0; i < number.length(); i++) {
-            char ch = number.toUpperCase().charAt(i);
-            minRadix = digits.indexOf(ch);
-            if (digits.contains(number.substring(i, i + 1))) {
-                if (minRadix > ch) {
-                    minRadix = i;
+        } else {
+            int minRadix = digits.indexOf(number.toUpperCase().charAt(0));
+            for (int i = 0; i < number.length(); i++) {
+                int ch = number.toUpperCase().charAt(i);
+                minRadix = digits.indexOf(ch) + 1;
+                if (digits.contains(ch + "")) {
+                    if (minRadix > digits.indexOf(ch) + 1) {
+                        minRadix = digits.indexOf(ch) + 1;
+                    }
                 }
             }
+            minRadix = minRadix < 2 ? 2 : minRadix;
+            System.out.println(minRadix);
         }
-        return minRadix + "";
     }
 
-
+    private static void findSolution(String line) {
+        Pattern pattern = Pattern.compile("[^0-9A-Za-z]");
+        if (pattern.matcher(line).find()) {
+            System.out.println("incorrect");
+        } else {
+            char max = 0;
+            char[] chars = line.toUpperCase().toCharArray();
+            for (char current : chars) {
+                if (current > max) {
+                    max = current;
+                }
+            }
+            int radix;
+            if (max <= '9') {
+                radix = max - 47;
+            } else {
+                radix = max - 54;
+            }
+            if (radix < 2) {
+                radix = 2;
+            }
+            System.out.println(radix);
+        }
+    }
 }
