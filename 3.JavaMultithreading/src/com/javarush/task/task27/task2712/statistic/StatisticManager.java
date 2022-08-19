5ef610c7508ed33992ced25afd6e8870b1232201
Agent7799 <agent7799@mail.ru>
2022-08-19 14:58:50 +0300

    2712 Restaurant 13/23 - StatisticStorage

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,23 +1,42 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
 
     private static StatisticManager instance;
+    private StatisticStorage statisticStorage = new StatisticStorage();
 
-    private StatisticManager(){}
-
+    //singleton
+    private StatisticManager() {
+    }
     public static StatisticManager getInstance() {
-        if (instance == null){
-             instance = new StatisticManager();
+        if (instance == null) {
+            instance = new StatisticManager();
         }
         return instance;
     }
 
+    //private inner class
+    private class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticStorage() {
+            //initializing storage
+            for (EventType eventType : EventType.values()) {
+                storage.put(eventType, new ArrayList<EventDataRow>());
+            }
+        }
+    }
 
 
-    public void register(EventDataRow data){
+    public void register(EventDataRow data) {
 
     }
 
